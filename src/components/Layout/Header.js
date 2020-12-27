3b2d2f6233a47d8f10507cccfaa1144d4970b263
Selin Öser <selinoser@gmail.com>
2020-12-27 04:23:24 +0300

    small changes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -14,7 +14,7 @@ const Header = () => {
                         <div className="row">
                             <div className="col-12">
                                 <nav className="navbar header__content">
-                                    <NavLink exact to="/" className="header__logo white">
+                                    <NavLink exact to={process.env.PUBLIC_URL + "/"} className="header__logo white">
                                         <h1 className=""><span style={txtColor}>RE</span>MOVIE<span style={txtColor}>ACT</span></h1>
                                     </NavLink>
                                     <button className="navbar-toggler header__btn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,10 +25,10 @@ const Header = () => {
                                     <div className="collapse navbar-collapse" id="navbarNav">
                                         <div className="header__line"></div>
                                         <ul class="navbar-nav header__nav">
-                                            <li className="header__nav-item nav-item"><NavLink exact to="/populer"  className="header__nav-link">POPÜLER</NavLink></li>
-                                            <li className="header__nav-item nav-item"><NavLink exact to="/top-rated" className="header__nav-link">EN ÇOK BEĞENİLENLER</NavLink></li>
-                                            <li className="header__nav-item nav-item"><NavLink exact to="/upcoming" className="header__nav-link">GELECEK FİLMLER</NavLink></li>
-                                            <li className="header__nav-item nav-item"><NavLink exact to="/contact" className="header__nav-link">İLETİŞİM</NavLink></li>
+                                            <li className="header__nav-item nav-item"><NavLink exact to={process.env.PUBLIC_URL + "/populer"}  className="header__nav-link">POPÜLER</NavLink></li>
+                                            <li className="header__nav-item nav-item"><NavLink exact to={process.env.PUBLIC_URL + "/top-rated"} className="header__nav-link">EN ÇOK BEĞENİLENLER</NavLink></li>
+                                            <li className="header__nav-item nav-item"><NavLink exact to={process.env.PUBLIC_URL + "/upcoming"} className="header__nav-link">GELECEK FİLMLER</NavLink></li>
+                                            <li className="header__nav-item nav-item"><NavLink exact to={process.env.PUBLIC_URL + "/contact"} className="header__nav-link">İLETİŞİM</NavLink></li>
                                         </ul>
                                     </div>
                                 </nav>
